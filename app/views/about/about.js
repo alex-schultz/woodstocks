3870ee9316b0dba0eef9b30f77bbe4191ca88223
alex-schultz <schultzy911@yahoo.com>
2016-08-25 14:50:22 -0700

    woodstocks with angular, express, and node

---
+++ app/views/about/about.js
@@ -0,0 +1,10 @@
+angular.module('').directive('about', function(){
+  return {
+    restrict: 'E',
+    templateUrl: '',
+    scope: {},
+    link: function(scope){
+      
+    }
+  };
+});
---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.use('/static', express.static(__dirname + '/app'));
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/app/index.html');
+});
+
+app.listen(3000, function(){
+  console.log('Server is up and running');
+});
