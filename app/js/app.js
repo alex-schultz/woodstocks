93412d80afe9a3bab54468b452ef422061cf67ce
alex-schultz <schultzy911@yahoo.com>
2016-08-26 13:38:38 -0700

    updated page to use angular ui router. Imported bootstrap, ui router, bower, angular, and jquery

---
+++ app/js/app.js
@@ -0,0 +1,67 @@
+(function() {
+  //include ui.router into what angular loads for the app
+  angular.module('pizza', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider, $locationProvider){
+      $urlRouterProvider.otherwise("/");
+      $locationProvider.html5Mode(true); //
+
+      // states Menu, coupons, suds, tour, donation, job, about
+      $stateProvider
+        .state('home', {
+          url: "/",
+          templateUrl: "static/views/home.html",
+          controller: function($scope){
+
+          }
+        })
+        .state('menu', {
+          url: "/menu",
+          templateUrl: "static/views/menu.html",
+          controller: function($scope){
+
+          }
+        })
+        .state('coupons', {
+          url: "/coupons",
+          templateUrl: "static/views/coupons.html",
+          controller: function($scope){
+
+          }
+        })
+        .state('suds', {
+          url: "/suds",
+          templateUrl: "static/views/suds.html",
+          controller: function($scope){
+
+          }
+        })
+        .state('tour', {
+          url: "/tour",
+          templateUrl: "static/views/tour.html",
+          controller: function($scope){
+
+          }
+        })
+        .state('donation', {
+          url: "/donation",
+          templateUrl: "static/views/donation.html",
+          controller: function($scope){
+
+          }
+        })
+        .state('job', {
+          url: '/job',
+          templateUrl: "static/views/job.html",
+          controller: function($scope){
+
+          }
+        })
+        .state('about', {
+          url: "/about",
+          templateUrl: "static/views/about/about.html",
+          controller: function($scope){
+
+          }
+        });
+    });
+})();
--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 
 app.use('/static', express.static(__dirname + '/app'));
+
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/app/index.html');
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log('Server is up and running');
 });
