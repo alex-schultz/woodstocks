0b4a7852611b5ddb79a2e4b8623b14b43cffaf60
Alex Schultz <schultzyy911@gmail.com>
2016-09-28 22:58:47 -0700

    finished menu. Added pictures for tour, finished coupons, suds, and donation requests

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+
 var express = require('express');
 var app = express();
 
 app.use('/static', express.static(__dirname + '/app'));
+
 
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/app/index.html');
 });
 
 app.listen(3000, function() {
-  console.log('Server is up and running');
+  console.log('Server is up and running on port 3000');
 });
